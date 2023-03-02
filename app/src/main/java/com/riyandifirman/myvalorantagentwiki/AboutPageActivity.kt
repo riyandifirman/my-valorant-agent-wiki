330b1b8d9cddf6b6435048d74ce77a985237e412
riyandifirman <riyandi.firman08@gmail.com>
2023-03-03 03:02:19 +0700

    add view binding, change image view with image button in activity_about_page.xml, and complete logical of AboutPageActivity.kt

--- app/src/main/java/com/riyandifirman/myvalorantagentwiki/AboutPageActivity.kt
+++ app/src/main/java/com/riyandifirman/myvalorantagentwiki/AboutPageActivity.kt
@@ -1,11 +1,31 @@
 package com.riyandifirman.myvalorantagentwiki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.riyandifirman.myvalorantagentwiki.databinding.ActivityAboutPageBinding
 
-class AboutPageActivity : AppCompatActivity() {
+class AboutPageActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityAboutPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_page)
+
+        binding = ActivityAboutPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibBack.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.ib_back -> {
+                val backIntent = Intent(this@AboutPageActivity, MainActivity::class.java)
+                startActivity(backIntent)
+            }
+        }
     }
 }
